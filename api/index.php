8a17347868d07bf5679ab72b232d44005ab1f59d
Green Darma Pharmaceuticals <greendarmapharmaceuticals@gmail.com>
2026-07-26 18:15:30 +0600

    fix: add APP_KEY, SQLite database and serverless database handler for Vercel

--- api/index.php
+++ api/index.php
@@ -15,9 +15,29 @@ foreach ($dirs as $dir) {
     }
 }
 
+// Copy sqlite database to /tmp if present for writable database access in Serverless
+$sqliteSource = __DIR__ . '/../database/database.sqlite';
+$sqliteTmp = sys_get_temp_dir() . '/database.sqlite';
+if (file_exists($sqliteSource) && !file_exists($sqliteTmp)) {
+    @copy($sqliteSource, $sqliteTmp);
+}
+
 // Override storage & compiled view locations for serverless execution
 putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
 $_ENV['VIEW_COMPILED_PATH'] = "{$storagePath}/framework/views";
 
+if (file_exists($sqliteTmp)) {
+    putenv("DB_CONNECTION=sqlite");
+    $_ENV['DB_CONNECTION'] = 'sqlite';
+    putenv("DB_DATABASE={$sqliteTmp}");
+    $_ENV['DB_DATABASE'] = $sqliteTmp;
+} elseif (file_exists($sqliteSource)) {
+    putenv("DB_CONNECTION=sqlite");
+    $_ENV['DB_CONNECTION'] = 'sqlite';
+    putenv("DB_DATABASE={$sqliteSource}");
+    $_ENV['DB_DATABASE'] = $sqliteSource;
+}
+
 // Forward Vercel request to Laravel entrypoint
 require __DIR__ . '/../public/index.php';
+

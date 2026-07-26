6c40e476898e5e545c933b8d694e44d15dc1461c
Green Darma Pharmaceuticals <greendarmapharmaceuticals@gmail.com>
2026-07-26 21:41:48 +0600

    fix: remove windows bootstrap cache files and set writable tmp cache paths for Vercel

--- api/index.php
+++ api/index.php
@@ -8,6 +8,7 @@ $dirs = [
     $storagePath . '/framework/sessions',
     $storagePath . '/framework/testing',
     $storagePath . '/logs',
+    $storagePath . '/bootstrap/cache',
 ];
 
 foreach ($dirs as $dir) {
@@ -23,6 +24,27 @@ $_SERVER['LARAVEL_STORAGE_PATH'] = $storagePath;
 
 putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
 $_ENV['VIEW_COMPILED_PATH'] = "{$storagePath}/framework/views";
+$_SERVER['VIEW_COMPILED_PATH'] = "{$storagePath}/framework/views";
+
+putenv("APP_SERVICES_CACHE={$storagePath}/bootstrap/cache/services.php");
+$_ENV['APP_SERVICES_CACHE'] = "{$storagePath}/bootstrap/cache/services.php";
+$_SERVER['APP_SERVICES_CACHE'] = "{$storagePath}/bootstrap/cache/services.php";
+
+putenv("APP_PACKAGES_CACHE={$storagePath}/bootstrap/cache/packages.php");
+$_ENV['APP_PACKAGES_CACHE'] = "{$storagePath}/bootstrap/cache/packages.php";
+$_SERVER['APP_PACKAGES_CACHE'] = "{$storagePath}/bootstrap/cache/packages.php";
+
+putenv("APP_CONFIG_CACHE={$storagePath}/bootstrap/cache/config.php");
+$_ENV['APP_CONFIG_CACHE'] = "{$storagePath}/bootstrap/cache/config.php";
+$_SERVER['APP_CONFIG_CACHE'] = "{$storagePath}/bootstrap/cache/config.php";
+
+putenv("APP_ROUTES_CACHE={$storagePath}/bootstrap/cache/routes.php");
+$_ENV['APP_ROUTES_CACHE'] = "{$storagePath}/bootstrap/cache/routes.php";
+$_SERVER['APP_ROUTES_CACHE'] = "{$storagePath}/bootstrap/cache/routes.php";
+
+putenv("APP_EVENTS_CACHE={$storagePath}/bootstrap/cache/events.php");
+$_ENV['APP_EVENTS_CACHE'] = "{$storagePath}/bootstrap/cache/events.php";
+$_SERVER['APP_EVENTS_CACHE'] = "{$storagePath}/bootstrap/cache/events.php";
 
 // Copy sqlite database to /tmp for serverless execution
 $sqliteSource = __DIR__ . '/../database/database.sqlite';
